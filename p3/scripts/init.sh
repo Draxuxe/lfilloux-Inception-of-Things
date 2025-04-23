2a41dfac71435794525aa7f73863d4c119662abf
Louis Filloux <lfilloux@z1r3p3.42lyon.fr>
2025-04-23 14:46:51 +0200

    Finish p2 and p3 continue

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -1,26 +1,17 @@
-k3d cluster create iot-cluster
-kubectl config use-context k3d-argocluster
+k3d cluster create iot-cluster --wait
 
 kubectl create namespace argocd
 kubectl create namespace dev
 
-kubectl apply -n argocd \
-  -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n argocd -f ./confs/argocd.yaml
 
 kubectl patch svc argocd-server -n argocd \
-  -p '{"spec": {"type": "NodePort", "ports":[{"port":443,"nodePort":32443,"targetPort":443}]}}'
+  -p '{"spec": {"type": "NodePort", "ports": [{"port": 443, "nodePort": 32443, "targetPort": 8080}]}}'
 
-# admin password = name of the server pod
-ARGO_POD=$(kubectl get pods -n argocd -l app.kubernetes.io/name=argocd-server \
-           -o name | cut -d/ -f2)
-kubectl -n argocd logs "argocd-server-$ARGO_POD" | grep 'password: ' | awk '{print $2}'
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > .argocd_pass
 
-argocd login localhost:32443 \
-  --username admin \
-  --password $(kubectl -n argocd get secret argocd-initial-admin-secret \
-    -o jsonpath="{.data.password}" | base64 -d) \
-  --insecure
+kubectl wait --for=condition=Ready pods --all -n argocd
+kubectl -n argocd port-forward svc/argocd-server 8080:443 > /dev/null 2>&1 &
 
-kubectl apply -f ./confs/argocd.yaml -n argocd
-
-argocd app sync playground-app
+kubectl port-forward svc/wil-service -n dev 8888:8888
