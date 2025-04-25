8d0e956770d93fe5d3cb8cc56dd13375a8b863d6
Louis Filloux <lfilloux@z1r3p1.42lyon.fr>
2025-04-25 09:41:02 +0200

    Modif script to wait admin_secret

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -9,9 +9,9 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl apply -n argocd -f ./confs/argocd.yaml
 kubectl apply -n dev -f ./confs/dev/deployement.yaml
 
+kubectl wait --for=condition=Ready pods --all -n argocd
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d > .argocd_pass
 
-kubectl wait --for=condition=Ready pods --all -n argocd
 kubectl -n argocd port-forward svc/argocd-server 8080:443 > /dev/null 2>&1 &
 
 kubectl port-forward svc/wil-service -n dev 8888:8888
